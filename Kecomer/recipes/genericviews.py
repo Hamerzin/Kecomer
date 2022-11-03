cfa92f626b0f09954aeda1c95ab1fa919609b9ea
hamerzin <dipaolo@gmail.com>
2022-11-02 22:02:36 -0300

    fix list

--- Kecomer/recipes/genericviews.py
+++ Kecomer/recipes/genericviews.py
@@ -12,14 +12,6 @@ class RecipesViewset(ModelViewSet):
 
     @swagger_auto_schema(operation_summary="List Product", operation_description="This endpoint list a users")
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
+        queryset =self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
-        filter_queryset = self.filter_jefatura(serializer)
-        return Response(filter_queryset)
-
-    def filter_jefatura(self, serializer):
-        new_query = defaultdict(list)
-        for row in serializer.data:
-            category = row.get('category')
-            new_query[category].append(row)
-        return new_query
+        return Response(serializer.data)
